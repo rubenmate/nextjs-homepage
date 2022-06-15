e90f8a2ed615c71332a1656e7419b41a0f986b9c
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-06-15 20:45:06 +0200

    Add Main layout

---
+++ components/layouts/main.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import { Box, Container } from '@chakra-ui/react'
+
+const Main = ({ children, router }) => {
+    return (
+        <Box as='main' pb={10}>
+            <Head>
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1'
+                />
+                <title>RubenMate</title>
+            </Head>
+            <Container maxW='container.md' pt={12}>
+                {children}
+            </Container>
+        </Box>
+    )
+}
+
+export default Main
