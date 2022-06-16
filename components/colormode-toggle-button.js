8879c0d98a365274592170ad2440a1011ff225f6
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-06-16 11:57:47 +0200

    Fix colormode button

--- components/colormode-toggle-button.js
+++ components/colormode-toggle-button.js
@@ -1,13 +1,15 @@
 import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 
+// TODO: Add animations with Framer-motion
 const ColorModeToggleButton = () => {
     const { toggleColorMode } = useColorMode()
     return (
         <IconButton
             aria-label='Toggle color mode'
-            icon={useColorModeValue(<SunIcon />, <MoonIcon />)}
+            icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
             onClick={toggleColorMode}
+            colorScheme={useColorModeValue('purple', 'orange')}
         ></IconButton>
     )
 }
