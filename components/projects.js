ec28c10cb4f979c673b3a139bd8cb6f4653c11d9
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-06-16 17:18:55 +0200

    Remove paragraph component and add Projects component

---
+++ components/projects.js
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react'
+import styled from '@emotion/styled'
+
+export const ProjectTitle = styled.span`
+    margin-right: 0.5em;
+    font-weight: bold;
+`
+
+export const Project = styled(Box)`
+    margin-top: 1em;
+    margin-botton: 1em;
+`
