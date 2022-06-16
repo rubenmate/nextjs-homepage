357b42b9b638e566c408dbbebc777a444496dff3
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-06-16 10:07:22 +0200

    Add IntroCard component and add it to index page

---
+++ components/intro-card.js
@@ -0,0 +1,19 @@
+import { Box, useColorModeValue } from '@chakra-ui/react'
+
+const IntroCard = ({ children }) => {
+    return (
+        <Box
+            p={4}
+            mb={6}
+            mt={3}
+            textAlign='center'
+            bg={useColorModeValue('#6B46C1', '#8be9fd')}
+            textColor={useColorModeValue('white', 'navy')}
+            borderRadius='lg'
+        >
+            {children}
+        </Box>
+    )
+}
+
+export default IntroCard
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import { Container } from '@chakra-ui/react'
 import Footer from '../components/footer'
+import IntroCard from '../components/intro-card'
 
 const Page = () => {
     return (
         <Container>
+            <IntroCard>
+                Hello, my name is Rubén and I&apos;m a computer science student
+                from Burgos, Spain. I love coding and I love doing it on Neovim.
+            </IntroCard>
             <Footer />
         </Container>
     )
