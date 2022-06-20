ebc01f68a389551afdc7f1843de94dbdcaeaf968
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-06-20 22:12:25 +0200

    fix(footer): fix mobile browsers cropping bottom

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# README
+
+## TODO
+
+-   Support larger screens
+-   Fix footer to bottom when there is not enought content
+-   Add posts page and button arrow pointing to it
+-   Add Rick Roll in Error 404 page
--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,24 +1,27 @@
 import Head from 'next/head'
 import { Box, Container } from '@chakra-ui/react'
+import Div100vh from 'react-div-100vh'
 import Navbar from '../navbar'
 import Footer from '../footer'
 
 const Main = ({ children, router }) => {
     return (
-        <Box as='main' height='100vh' display='flex' flexDir='column'>
-            <Head>
-                <meta
-                    name='viewport'
-                    content='width=device-width, initial-scale=1'
-                />
-                <title>ruben-mate</title>
-            </Head>
-            <Navbar path={router.asPath} />
-            <Container maxW='container.md' pt={12}>
-                {children}
-            </Container>
-            <Footer />
-        </Box>
+        <Div100vh>
+            <Box as='main' height='full' display='flex' flexDir='column'>
+                <Head>
+                    <meta
+                        name='viewport'
+                        content='width=device-width, initial-scale=1'
+                    />
+                    <title>ruben-mate</title>
+                </Head>
+                <Navbar path={router.asPath} />
+                <Container maxW='container.md' pt={12}>
+                    {children}
+                </Container>
+                <Footer />
+            </Box>
+        </Div100vh>
     )
 }
 
