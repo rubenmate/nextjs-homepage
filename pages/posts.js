2a4c3a341cc74ad3035311ba03aad21019117b44
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-07-21 14:04:52 +0200

    feat: better styles in posts list page

--- pages/posts.js
+++ pages/posts.js
@@ -1,8 +1,6 @@
-import { useState } from 'react'
-
 import NextLink from 'next/link'
 
-const { Box, Container, Heading, background, Text } = require('@chakra-ui/react')
+const { Box, Container, Heading, Text } = require('@chakra-ui/react')
 import { getSortedPostsData } from '../utils/posts'
 
 export async function getStaticProps() {
@@ -26,35 +24,38 @@ const Posts = ({ allPostsData }) => {
     )
     const normalView = (
         <Box pt={2}>
-            <Heading as="h3" variant="title" pb={4}>
+            <Heading as="h3" variant="title" pb={6}>
                 Posts
             </Heading>
             {allPostsData.map(({ id, date, title, description }) => {
                 let dateToFormat = new Date(date)
                 return (
-                    <NextLink key={id} href={`/pages/${id}`} passHref scroll={false}>
-                        <Box
-                            as="article"
-                            justifySelf="start"
-                            cursor="pointer"
-                            _hover={{
-                                'background-color': '#80808010',
-                            }}
-                        >
-                            <Heading as="h4" variant="post-title">
-                                {title}
-                            </Heading>
-                            <Box as="p">{description}</Box>
-                            <Text fontSize="sm" pt={2}>
-                                {dateToFormat.toLocaleString('en-US', {
-                                    weekday: 'short',
-                                    day: 'numeric',
-                                    year: 'numeric',
-                                    month: 'long',
-                                })}
-                            </Text>
-                        </Box>
-                    </NextLink>
+                    <>
+                        <NextLink key={id} href={`/posts/${id}`} passHref scroll={false}>
+                            <Box
+                                as="article"
+                                justifySelf="start"
+                                cursor="pointer"
+                                _hover={{
+                                    'background-color': '#80808010',
+                                }}
+                            >
+                                <Heading as="h4" variant="post-title">
+                                    {title}
+                                </Heading>
+                                <Box as="p">{description}</Box>
+                                <Text fontSize="sm" pt={2} mb={4}>
+                                    {dateToFormat.toLocaleString('en-US', {
+                                        weekday: 'short',
+                                        day: 'numeric',
+                                        year: 'numeric',
+                                        month: 'long',
+                                    })}
+                                </Text>
+                            </Box>
+                        </NextLink>
+                        <Box mt={12} />
+                    </>
                 )
             })}
         </Box>
